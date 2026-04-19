6b1b6f23161d51406f97e88e9ac16fac74b2509f
Ash <161075247+zararashigal@users.noreply.github.com>
2026-04-19 10:46:13 +0200

    Add files via upload

--- ss2ms/assets/js/main.js
+++ ss2ms/assets/js/main.js
@@ -7,6 +7,26 @@ async function search4lobby () {
         }
         let result = await response.json();
         await document.getElementById("foundCode").classList.remove("hideRoom");
-        document.getElementById("foundCode").innerHTML = await `Found Lobby! Roomcode: ${result[Math.floor(Math.random() * result.length)]}`
+        document.getElementById("foundCode").innerHTML = await `Found Lobby! Roomcode: ${result[Math.floor(Math.random() * result.length)]}`;
+    } catch(_){}
+}
+
+async function hostLobby () {
+    let room = document.getElementById("room")
+    
+    if (room.value.length != 4)
+        return;
+
+    let endpoint = "https://shodan.zarashigal.eu.org/host?roomCode=";
+    try {
+        let response = await fetch(`${endpoint}${room.value.toUpperCase()}`);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        let result = await response;
+        await console.log(result);
+        await document.getElementById("hostedLobby").classList.remove("hideRoom");
+        document.getElementById("hostedLobby").innerHTML = await `Hosting room ${room.value.toUpperCase()} on matchmaking srv for others to search...`;
+        room.value = await "";
     } catch(_){}
 }
